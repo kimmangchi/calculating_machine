6d819f3a998129553688ccef9a366df225dd7c9b
kimmangchi <bbq5897@naver.com>
2024-12-23 12:00:47 +0900

    계산 기능 클래스 추가(구현 완료)

---
+++ calculate.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculating_machine
+{
+    class calculate
+    {
+        //더하기
+        public double addtion(string x, string y)
+        {
+            return double.Parse(x) + double.Parse(x);
+        }
+
+        //빼기
+        public double subtraction(string x, string y)
+        {
+            return double.Parse(x) - double.Parse(x);
+        }
+
+        //나누기
+        public double division(string x, string y)
+        {
+            return double.Parse(x) / double.Parse(x);
+        }
+
+        //곱하기
+        public double multiplication(string x, string y)
+        {
+            return double.Parse(x) * double.Parse(x);
+        }
+
+        // 제곱
+        public double GetSquare_Value(string x)
+        {
+            return double.Parse(x) * double.Parse(x);
+        }
+
+        // 제곱근
+        public double GetRoot_Value(string x)
+        {
+            double result = Math.Pow(double.Parse(x), 0.5);
+            return result;
+
+        }
+
+        // 역수
+        public double GetInverse_Value(string x)
+        {
+            if (double.Parse(x) == 0)
+            {
+                return double.NaN;
+            }
+            else
+            {
+                double result = 1 / double.Parse(x);
+                return result;
+            }
+        }
+
+        // 퍼센트(%)
+        public double GetPercentage_Value(string x)
+        {
+            double result = double.Parse(x) / 100;
+            return result;
+        }
+
+    }
+}
